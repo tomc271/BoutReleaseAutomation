220dd05f62f6df7e1c25305a558340b2aa745392
tomc271 <tomc271@gmail.com>
2023-07-03 10:28:48 +0100

    Added update_version_number_in_files.py

---
+++ update_version_number_in_files.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import re
+
+
+def get_filepath(filepath):
+
+    return Path("C:\\git\\BOUT-dev") / filepath
+
+
+def update_version_number_in_files(new_version_number):
+
+    filepath = get_filepath("configure.ac")
+    with open(filepath, "r", encoding='UTF-8') as file:
+        file_contents = file.read()
+
+        pattern = "AC_INIT\\(\\[BOUT\\+\\+\\],\\[(\\d\\.\\d\\.\\d)\\]"
+        replacement = "AC_INIT([BOUT++],[" + new_version_number + "]"
+        updated_text = re.sub(pattern, replacement, file_contents)
+    with open(filepath, "w", encoding='UTF-8') as file:
+        file.write(updated_text)
+
+
+if __name__ == '__main__':
+    update_version_number_in_files("6.1.2")
