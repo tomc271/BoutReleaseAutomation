13db08bb96e2a3c951fbb0d1b4f080f7df862ab4
tomc271 <tomc271@gmail.com>
2023-07-03 12:21:51 +0100

    Added dictionary files_with_regex_patterns.

--- update_version_number_in_files.py
+++ update_version_number_in_files.py
@@ -7,19 +7,24 @@ def get_full_filepath(filepath):
     return Path(r"C:\git\BOUT-dev") / filepath
 
 
-def update_version_number_in_files(new_version_number):
+def update_version_number_in_files(new_version_number, files_with_regex_patterns):
 
-    full_filepath = get_full_filepath("configure.ac")
-    with open(full_filepath, "r", encoding='UTF-8') as file:
-        file_contents = file.read()
+    for filepath in files_with_regex_patterns:
+        pattern = files_with_regex_patterns[filepath]
+        full_filepath = get_full_filepath(filepath)
+        with open(full_filepath, "r", encoding='UTF-8') as file:
+            file_contents = file.read()
+            replacement = "AC_INIT([BOUT++],[" + new_version_number + "]"
+            updated_text = re.sub(pattern, replacement, file_contents)
 
-        pattern = r"AC_INIT\(\[BOUT\+\+\],\[(\d\.\d\.\d)\]"
-        replacement = "AC_INIT([BOUT++],[" + new_version_number + "]"
-        updated_text = re.sub(pattern, replacement, file_contents)
-    with open(full_filepath, "w", encoding='UTF-8') as file:
-        file.write(updated_text)
+        with open(full_filepath, "w", encoding='UTF-8') as file:
+            file.write(updated_text)
 
 
 if __name__ == '__main__':
 
-    update_version_number_in_files("6.1.2")
+    files_with_regex_patterns = {
+        "configure.ac": r"AC_INIT\(\[BOUT\+\+\],\[(\d\.\d\.\d)\]"
+    }
+
+    update_version_number_in_files("6.1.2", files_with_regex_patterns)
