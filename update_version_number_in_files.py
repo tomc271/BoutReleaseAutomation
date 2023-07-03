86c6493cdcae73633042cc0998d7555d3c00636d
tomc271 <tomc271@gmail.com>
2023-07-03 12:21:08 +0100

    Rename get_filepath() to get_full_filepath().

--- update_version_number_in_files.py
+++ update_version_number_in_files.py
@@ -2,21 +2,21 @@ from pathlib import Path
 import re
 
 
-def get_filepath(filepath):
+def get_full_filepath(filepath):
 
     return Path(r"C:\git\BOUT-dev") / filepath
 
 
 def update_version_number_in_files(new_version_number):
 
-    filepath = get_filepath("configure.ac")
-    with open(filepath, "r", encoding='UTF-8') as file:
+    full_filepath = get_full_filepath("configure.ac")
+    with open(full_filepath, "r", encoding='UTF-8') as file:
         file_contents = file.read()
 
         pattern = r"AC_INIT\(\[BOUT\+\+\],\[(\d\.\d\.\d)\]"
         replacement = "AC_INIT([BOUT++],[" + new_version_number + "]"
         updated_text = re.sub(pattern, replacement, file_contents)
-    with open(filepath, "w", encoding='UTF-8') as file:
+    with open(full_filepath, "w", encoding='UTF-8') as file:
         file.write(updated_text)
 
 
