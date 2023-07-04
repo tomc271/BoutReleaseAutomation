8693e92914c79c8e738440ed49bffa2374088886
tomc271 <tomc271@gmail.com>
2023-07-04 14:34:09 +0100

    Call get_full_filepath() inside update_version_number_in_file() function

--- update_version_number_in_files.py
+++ update_version_number_in_files.py
@@ -7,8 +7,9 @@ def get_full_filepath(filepath):
     return Path(r"C:\git\BOUT-dev") / filepath
 
 
-def update_version_number_in_file(full_filepath, pattern, new_version_number):
+def update_version_number_in_file(relative_filepath, pattern, new_version_number):
 
+    full_filepath = get_full_filepath(relative_filepath)
     with open(full_filepath, "r", encoding='UTF-8') as file:
         file_contents = file.read()
         updated_text = re.sub(pattern, new_version_number, file_contents)
@@ -23,40 +24,26 @@ def bump_version_numbers(new_version_number):
     new_minor_version_number = str(int(new_version_number[2]) + 1)
     bout_next_version_number = re.sub(r"(?<=\d\.)\d(?=\.\d)", new_minor_version_number, new_version_number)
 
-    update_version_number_in_file(
-        get_full_filepath("configure.ac"), r"(?<=AC_INIT\(\[BOUT\+\+\],\[)\d\.\d\.\d(?=\])", new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("CITATION.cff"), r"(?<=version: )\d\.\d\.\d", new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("manual/sphinx/conf.py"), r"(?<=version = \")\d\.\d(?=\")", short_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("manual/sphinx/conf.py"), r"(?<=release = \")\d\.\d\.\d(?=\")", new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("manual/doxygen/Doxyfile_readthedocs"), r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d",
-        new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("manual/doxygen/Doxyfile"), r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d", new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("CMakeLists.txt"), r"(?<=set\(_bout_previous_version \"v)\d\.\d\.\d(?=\"\))",
-        new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("CMakeLists.txt"), r"(?<=set\(_bout_next_version \")\d\.\d\.\d(?=\"\))",
-        bout_next_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("tools/pylib/_boutpp_build/backend.py"), r"(?<=_bout_previous_version = \"v)\d\.\d\.\d(?=\")",
-        new_version_number)
-
-    update_version_number_in_file(
-        get_full_filepath("tools/pylib/_boutpp_build/backend.py"), r"(?<=_bout_next_version = \"v)\d\.\d\.\d(?=\")",
-        bout_next_version_number)
+    update_version_number_in_file("configure.ac",
+                                  r"(?<=AC_INIT\(\[BOUT\+\+\],\[)\d\.\d\.\d(?=\])", new_version_number)
+    update_version_number_in_file("CITATION.cff",
+                                  r"(?<=version: )\d\.\d\.\d", new_version_number)
+    update_version_number_in_file("manual/sphinx/conf.py",
+                                  r"(?<=version = \")\d\.\d(?=\")", short_version_number)
+    update_version_number_in_file("manual/sphinx/conf.py",
+                                  r"(?<=release = \")\d\.\d\.\d(?=\")", new_version_number)
+    update_version_number_in_file("manual/doxygen/Doxyfile_readthedocs",
+                                  r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d", new_version_number)
+    update_version_number_in_file("manual/doxygen/Doxyfile",
+                                  r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d", new_version_number)
+    update_version_number_in_file("CMakeLists.txt",
+                                  r"(?<=set\(_bout_previous_version \"v)\d\.\d\.\d(?=\"\))", new_version_number)
+    update_version_number_in_file("CMakeLists.txt",
+                                  r"(?<=set\(_bout_next_version \")\d\.\d\.\d(?=\"\))", bout_next_version_number)
+    update_version_number_in_file("tools/pylib/_boutpp_build/backend.py",
+                                  r"(?<=_bout_previous_version = \"v)\d\.\d\.\d(?=\")", new_version_number)
+    update_version_number_in_file("tools/pylib/_boutpp_build/backend.py",
+                                  r"(?<=_bout_next_version = \"v)\d\.\d\.\d(?=\")", bout_next_version_number)
 
 
 if __name__ == '__main__':
