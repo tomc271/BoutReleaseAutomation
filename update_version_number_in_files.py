d742dd3465004cf58883f204bf8eac23e319d922
tomc271 <tomc271@gmail.com>
2023-07-04 09:35:09 +0100

    Iterate over files in outer function.

--- update_version_number_in_files.py
+++ update_version_number_in_files.py
@@ -7,19 +7,14 @@ def get_full_filepath(filepath):
     return Path(r"C:\git\BOUT-dev") / filepath
 
 
-def update_version_number_in_files(new_version_number, files_with_regex_patterns):
+def update_version_number_in_file(new_version_number, pattern, full_filepath):
 
-    for filepath in files_with_regex_patterns:
+    with open(full_filepath, "r", encoding='UTF-8') as file:
+        file_contents = file.read()
+        updated_text = re.sub(pattern, new_version_number, file_contents)
 
-        pattern = files_with_regex_patterns[filepath]
-        full_filepath = get_full_filepath(filepath)
-
-        with open(full_filepath, "r", encoding='UTF-8') as file:
-            file_contents = file.read()
-            updated_text = re.sub(pattern, new_version_number, file_contents)
-
-        with open(full_filepath, "w", encoding='UTF-8') as file:
-            file.write(updated_text)
+    with open(full_filepath, "w", encoding='UTF-8') as file:
+        file.write(updated_text)
 
 
 if __name__ == '__main__':
@@ -31,4 +26,9 @@ if __name__ == '__main__':
         "CITATION.cff": r"(?<=version: )\d\.\d\.\d"
     }
 
-    update_version_number_in_files(new_version_number, files_with_regex_patterns)
+    for filepath in files_with_regex_patterns:
+
+        pattern = files_with_regex_patterns[filepath]
+        full_filepath = get_full_filepath(filepath)
+
+        update_version_number_in_file(new_version_number, pattern, full_filepath)
