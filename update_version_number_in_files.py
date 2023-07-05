4af2ef46b90f6807a745c8afc561a74c6269a13e
tomc271 <tomc271@gmail.com>
2023-07-05 11:30:31 +0100

    Use start of line anchor (and re.MULTILINE flag) to avoid false matches. Use capture groups instead of lookbehinds and lookaheads, since won't work with start anchor.

--- update_version_number_in_files.py
+++ update_version_number_in_files.py
@@ -9,11 +9,13 @@ def get_full_filepath(filepath):
 
 def update_version_number_in_file(relative_filepath, pattern, new_version_number):
 
+    def get_replacement(match):
+        return match[0].replace(match[1], new_version_number)
+
     full_filepath = get_full_filepath(relative_filepath)
     with open(full_filepath, "r", encoding='UTF-8') as file:
         file_contents = file.read()
-        updated_text = re.sub(pattern, new_version_number, file_contents)
-
+        updated_text = re.sub(pattern, get_replacement, file_contents, flags=re.MULTILINE)
     with open(full_filepath, "w", encoding='UTF-8') as file:
         file.write(updated_text)
 
@@ -25,25 +27,25 @@ def bump_version_numbers(new_version_number):
     bout_next_version_number = re.sub(r"(?<=\d\.)\d(?=\.\d)", new_minor_version_number, new_version_number)
 
     update_version_number_in_file("configure.ac",
-                                  r"(?<=AC_INIT\(\[BOUT\+\+\],\[)\d\.\d\.\d(?=\])", new_version_number)
+                                  r"^AC_INIT\(\[BOUT\+\+\],\[(\d\.\d\.\d)\]", new_version_number)
     update_version_number_in_file("CITATION.cff",
-                                  r"(?<=version: )\d\.\d\.\d", new_version_number)
+                                  r"^version: (\d\.\d\.\d)", new_version_number)
     update_version_number_in_file("manual/sphinx/conf.py",
-                                  r"(?<=version = \")\d\.\d(?=\")", short_version_number)
+                                  r"^version = \"(\d\.\d)\"", short_version_number)
     update_version_number_in_file("manual/sphinx/conf.py",
-                                  r"(?<=release = \")\d\.\d\.\d(?=\")", new_version_number)
+                                  r"^release = \"(\d\.\d\.\d)\"", new_version_number)
     update_version_number_in_file("manual/doxygen/Doxyfile_readthedocs",
-                                  r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d", new_version_number)
+                                  r"^PROJECT_NUMBER         = (\d\.\d\.\d)", new_version_number)
     update_version_number_in_file("manual/doxygen/Doxyfile",
-                                  r"(?<=PROJECT_NUMBER         = )\d\.\d\.\d", new_version_number)
+                                  r"^PROJECT_NUMBER         = (\d\.\d\.\d)", new_version_number)
     update_version_number_in_file("CMakeLists.txt",
-                                  r"(?<=set\(_bout_previous_version \"v)\d\.\d\.\d(?=\"\))", new_version_number)
+                                  r"^set\(_bout_previous_version \"v(\d\.\d\.\d)\"\)", new_version_number)
     update_version_number_in_file("CMakeLists.txt",
-                                  r"(?<=set\(_bout_next_version \")\d\.\d\.\d(?=\"\))", bout_next_version_number)
+                                  r"^set\(_bout_next_version \"(\d\.\d\.\d)\"\)", bout_next_version_number)
     update_version_number_in_file("tools/pylib/_boutpp_build/backend.py",
-                                  r"(?<=_bout_previous_version = \"v)\d\.\d\.\d(?=\")", new_version_number)
+                                  r"_bout_previous_version = \"v(\d\.\d\.\d)\"", new_version_number)
     update_version_number_in_file("tools/pylib/_boutpp_build/backend.py",
-                                  r"(?<=_bout_next_version = \"v)\d\.\d\.\d(?=\")", bout_next_version_number)
+                                  r"_bout_next_version = \"v(\d\.\d\.\d)\"", bout_next_version_number)
 
 
 if __name__ == '__main__':
